f63e813b3b0623d6431d351ff5a99b8019009e93
Vincenzo Maria Schimmenti <35848915+Schimmenti@users.noreply.github.com>
2019-07-06 19:51:38 +0200

    Add files via upload

--- rk2alg.cpp
+++ rk2alg.cpp
@@ -17,7 +17,7 @@ double approxLogFact(double n)
   return (n+0.5)*n-n+0.5*log(2*M_PI);
 }
 
-int compute_instance_index(int n,int rowIndex, IntegerMatrix data, IntegerVector r, int pLen, IntegerVector p_i)
+int compute_instance_index(int n,int rowIndex, IntegerMatrix& data, IntegerVector& r, int pLen, IntegerVector& p_i)
 {
   int j = 0;
   int c = r[p_i[pLen-1]];
@@ -36,7 +36,7 @@ int compute_instance_index(int n,int rowIndex, IntegerMatrix data, IntegerVector
   return j;
 }
 
-IntegerMatrix compute_alpha(int i, int n, IntegerVector r, int pLen, IntegerVector p_i, int m, IntegerMatrix data, int& nrows)
+IntegerMatrix compute_alpha(int i, int n, IntegerVector& r, int pLen, IntegerVector& p_i, int m, IntegerMatrix& data, int& nrows)
 {
   nrows = 1;
   for(int l = 0; l < pLen; l++)
@@ -54,7 +54,7 @@ IntegerMatrix compute_alpha(int i, int n, IntegerVector r, int pLen, IntegerVect
   return alpha;
 }
 
-IntegerMatrix compute_alpha_nop(int i, int n, IntegerVector r, int m, IntegerMatrix data)
+IntegerMatrix compute_alpha_nop(int i, int n, IntegerVector& r, int m, IntegerMatrix& data)
 {
   int r_i = r[i];
   IntegerMatrix alpha(r_i,1);
@@ -65,7 +65,7 @@ IntegerMatrix compute_alpha_nop(int i, int n, IntegerVector r, int m, IntegerMat
   return alpha;
 }
 
-double compute_f_nop(int i, int n, IntegerVector r,  int m, IntegerMatrix data)
+double compute_f_nop(int i, int n, IntegerVector& r,  int m, IntegerMatrix& data)
 {
   IntegerMatrix alpha = compute_alpha_nop(i, n, r, m, data);
   int N = 0;
@@ -78,7 +78,7 @@ double compute_f_nop(int i, int n, IntegerVector r,  int m, IntegerMatrix data)
   return factorial(r[i]-1)*prod/factorial(N+r[i]-1);
 }
 const double exp_scale = 100000;
-double compute_f(int i, int n, IntegerVector r, int pLen, IntegerVector p_i, int m, IntegerMatrix data)
+double compute_f(int i, int n, IntegerVector& r, int pLen, IntegerVector& p_i, int m, IntegerMatrix& data)
 {
   int nrows = 0;
   IntegerMatrix alpha = compute_alpha(i,n,r,pLen, p_i, m, data, nrows);
@@ -110,7 +110,7 @@ double compute_f(int i, int n, IntegerVector r, int pLen, IntegerVector p_i, int
   return f;
 }
 
-IntegerVector k2alg(IntegerVector& cp, int u,int i, int n, IntegerVector r, int m, IntegerMatrix data)
+IntegerVector k2alg(IntegerVector& cp, double& score,int u,int i, int n, IntegerVector& r, int m, IntegerMatrix& data)
 {
   IntegerVector p;
   int pLen = 0;
@@ -152,6 +152,11 @@ IntegerVector k2alg(IntegerVector& cp, int u,int i, int n, IntegerVector r, int 
       
     }        
   }
+  score = pOld;
+  if(pLen==0)
+  {
+    p.push_back(-1);
+  }
   return p;
 }
 
@@ -180,7 +185,7 @@ bool checkDataset(IntegerMatrix data, IntegerVector r)
 
 
 // [[Rcpp::export]]
-List k2procedure(SEXP x,SEXP dims, SEXP varOrder, int u = -1)
+List k2procedure(SEXP x,SEXP dims, SEXP varOrder, NumericVector& scores, int u = -1)
 {
   IntegerMatrix data(x);
   IntegerVector order(varOrder);
@@ -214,37 +219,145 @@ List k2procedure(SEXP x,SEXP dims, SEXP varOrder, int u = -1)
     }
     if(cp.size()!=0)
     {
-      IntegerVector p_i = k2alg(cp,u,i,n,r,m,data);
+      double score = 0;
+      IntegerVector p_i = k2alg(cp,score,u,i,n,r,m,data);
+      scores.push_back(score);
+      std::sort(p_i.begin(), p_i.end());
       result.push_back(p_i);
     }
     else
     {
-      result.push_back(-1);
+      result.push_back(rep(-1,1));
     }
   }
   return result;
 }
 
-
-/*** R
-k2procedureSplit <- function(x, dims, initVarOder, u = -1, subSize = 500)
+// [[Rcpp::export]]
+List k2procedureSplit(SEXP x,SEXP dims, SEXP varOrder, int verbose = 0,int u = -1, int splitSize=100)
 {
-  nRows <- dim(x)[0]
-  nSplits <- nRows / subSize
-  vOrder <- initVarOder
-  for(i in 1:nSplits)
+  IntegerMatrix data(x);
+  IntegerVector order(varOrder);
+  IntegerVector r(dims);
+  int nRows = data.nrow();
+  int nCols = data.ncol();
+  int nSplits = nRows / splitSize;
+  double bestScore = 0;
+  int bestRichness = 0;
+  List bestList;
+  int from = 0;
+  for(int s = 0; s < nSplits; s++)
   {
-    lastRow <- i + subSize - 1
-    if(i==nSplits)
+    int sz = splitSize;
+    if(s==nSplits-1 && nRows % splitSize != 0)
     {
-      lastRow <- nRows
+      sz = nRows % splitSize;
     }
-    subData <- x[i:lastRow,]
-    res <- k2procedure(subData, dims, vOrder, u)
+    //now copy the data matrix
+    IntegerMatrix sData(sz, nCols);
+    for(int i = 0; i < sz*nCols; i++)
+    {
+      sData[i] = data[from+i];
+    }
+    from += sz*nCols;
     
+    NumericVector scs(nCols);
+    List res =  k2procedure(sData, r, order, scs, u);
+    
+    double totalSc = mean(scs);
+    int richness = 0;
+    for(int i = 0; i < nCols; i++)
+    {
+      if(res[i]!=nullptr)
+      {
+        IntegerVector temp(res[i]);
+        richness += temp.size();
+      }
+    }
+    if(verbose > 0 && s % verbose == 0)
+    {
+      Rcout << "Split n° " << (s+1) << " with size " << sz << " with score " << totalSc << endl;
+      //Rcpp:print(res);
+    }
+    if(totalSc+richness>bestScore+bestRichness)
+    {
+      bestScore = totalSc;
+      bestRichness = richness;
+      bestList = res;
+    }
   }
+  return bestList;
 }
-*/
+
+// [[Rcpp::export]]
+NumericMatrix conditionalProb(int i, IntegerMatrix x,IntegerVector dims, SEXP p_i)
+{
+  int nRows = 1;
+  int m = x.nrow();
+  int n= x.ncol();
+  IntegerVector parents(p_i);
+  int pLen = parents.size();
+  if(pLen >= n)
+  {
+    return NumericMatrix(0,0);
+  }
+  int r_i = dims[i];
+  if(pLen==0)
+  {
+    return NumericMatrix(0,0);
+  }
+  for(int l = 0; l < pLen; l++)
+  {
+    nRows *= dims[parents[l]];
+  }
+  NumericVector P(nRows*r_i);
+  IntegerVector counts(nRows);
+  for(int a = 0; a < m; a++)
+  {
+    int j = compute_instance_index(n, a, x, dims, pLen, parents); //the actual index in P is different
+    P[j*r_i+x(a,i)]++;
+    counts[j]++;
+  }
+  for(int j = 0; j < nRows; j++)
+  {
+    for(int l = 0; l < r_i; l++)
+    {
+      P[j*r_i+l] /= counts[j];
+    }
+  }
+  NumericMatrix T(nRows*r_i, 2+pLen);
+  CharacterVector names(2+pLen);
+  names[pLen] = "x" + to_string(i);
+  names[pLen+1] = "P";
+  for(int l = 0; l < pLen; l++)
+  {
+    names[l] = "x" + to_string(parents[l]);
+  }
+  colnames(T) = names;
+  for(int j = 0; j < nRows; j++)
+  {
+    for(int l = 0; l < r_i; l++)
+    {
+      T(j*r_i+l,pLen+1)=P[j*r_i+l];
+      T(j*r_i+l,pLen)=l;
+    }
+    int c = nRows;
+    for(int l = 0; l < pLen; l++)
+    {
+      c /= dims[parents[l]];
+      int val;
+
+      val = (j / c)%dims[parents[l]]; 
+      
+      for(int k = 0; k < r_i; k++)
+      {
+        T(j*r_i+k,l)=val;
+      }
+    }
+  }
+  return T;
+}
+
 
 
 
